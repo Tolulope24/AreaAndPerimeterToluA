3b3eb49a4f31772216b86c2daa83b1b4e777cbff
Tolulope24 <36515830+Tolulope24@users.noreply.github.com>
2018-02-20 13:56:34 -0500

    Initial commit

---
+++ AreaAndPerimeterForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace AreaAndPerimeterToluA
+{
+    partial class frmAreaAndPerimeter
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuFile = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mniexit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFile.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.mnuFile.Location = new System.Drawing.Point(0, 0);
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(284, 24);
+            this.mnuFile.TabIndex = 0;
+            this.mnuFile.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mniexit});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // mniexit
+            // 
+            this.mniexit.Name = "mniexit";
+            this.mniexit.Size = new System.Drawing.Size(152, 22);
+            this.mniexit.Text = "Exit";
+            this.mniexit.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // frmAreaAndPerimeter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.mnuFile);
+            this.MainMenuStrip = this.mnuFile;
+            this.Name = "frmAreaAndPerimeter";
+            this.Text = "Changing Area And Perimeter";
+            this.mnuFile.ResumeLayout(false);
+            this.mnuFile.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mniexit;
+    }
+}
+
